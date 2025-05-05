0c43aff2a06d9105d3f4ab3bd7aaf43507de9c90
ryhoneyman <ryhoneyman@gmail.com>
2025-05-05 18:43:45 +0000

    fixed prepare types for character insert

--- api/v1/core/models/logmodel.class.php
+++ api/v1/core/models/logmodel.class.php
@@ -93,7 +93,7 @@ class LogModel extends DefaultModel
          $statement = "INSERT INTO character_data (account_id,name,updated,".implode(',',array_keys($updateFields)).") ". 
                       "VALUES (?,?,now(),".implode(',',array_fill(0,count($updateFields),'?')).") ".
                       "ON DUPLICATE KEY UPDATE updated=values(updated), ".implode(', ',array_map(function($field) { return "$field=values($field)"; },array_keys($updateFields)));
-         $types     = 'iss'.implode('',array_column($updateFields,'type'));
+         $types     = 'is'.implode('',array_column($updateFields,'type'));
          $data      = array_merge([$accountInfo['id'],$characterName],array_column($updateFields,'value'));
 
          $this->debug(0,"STATEMENT: $statement");
