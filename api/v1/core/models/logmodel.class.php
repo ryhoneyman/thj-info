50c21cbf9a3e3d2ea938c2d1054fc6e4c572c9cc
ryhoneyman <ryhoneyman@gmail.com>
2025-05-20 14:14:03 +0000

    double tic escape messages

--- api/v1/core/models/logmodel.class.php
+++ api/v1/core/models/logmodel.class.php
@@ -85,10 +85,10 @@ class LogModel extends DefaultModel
                $alertMessage = null;
 
                if ($columnKey == 'powerslot_percent' && $updateValue >= 100 && preg_match('/\(enchanted\)$/i',$info['powerslot_item'])) {
-                  $alertMessage = sprintf("You have finished `%s` from the powerslot on `%s`",str_replace('(Enchanted)','(Legendary)',$info['powerslot_item']),$characterName);
+                  $alertMessage = sprintf("You have finished ``%s`` from the powerslot on ``%s``",str_replace('(Enchanted)','(Legendary)',$info['powerslot_item']),$characterName);
                }
                else if ($columnKey == 'aa_points' && $updateValue >= 100) {
-                  $alertMessage = sprintf("You have reached `%s` ability points on `%s`",$updateValue,$characterName);
+                  $alertMessage = sprintf("You have reached ``%s`` ability points on ``%s``",$updateValue,$characterName);
                }
 
                if ($alertMessage && isset($accountInfo['discord_id'])) {
